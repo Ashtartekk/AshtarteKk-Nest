fbb6cdae8a14711bfe4f39c73506a618be259e23
AshtarteKk <865189421@qq.com>
2022-12-08 19:58:47 +0800

    登录验证码

--- src/module/admin/login/login.controller.ts
+++ src/module/admin/login/login.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Request, Response } from '@nestjs/common';
+import { ToolsService } from '../../../service/tools/tools.service';
 
 @Controller('admin/login')
 export class LoginController {
+  constructor(private toolservice: ToolsService) {}
   @Get()
   @Render('admin/login')
   index() {
     return {};
+  }
+
+  @Get('code')
+  getCode(@Request() req, @Response() res) {
+    const svgCaptcha = this.toolservice.getcaptcha();
+    //设置session
+    req.session.code = svgCaptcha.text;
+    res.type('svg');
+    res.send(svgCaptcha.data);
   }
 }
